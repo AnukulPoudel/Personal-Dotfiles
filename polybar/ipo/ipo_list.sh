0e8b431c7e28febcbe7d13110ca72663a89e9d58
arch-anukul <anukulpoudel999@gmail.com>
2025-04-29 21:12:17 +0545

    removed ipo and calender, updated config

--- polybar/ipo/ipo_list.sh
+++
@@ -1,8 +0,0 @@
-!/bin/bash
-
-OpenIPO = #open ipo list
-
-ARR = ()
-ARR += ("IPO")
-ARR += ("$OpenIPO")
-ARR += ("$UpcommingIPO")
--- polybar/ipo/ipo_module.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Execute the Python script to get the count of available IPOs
-IPO_COUNT=$(python3 /home/anukul/.config/polybar/ipo/ipo_module.py)
-
-# Output the IPO count
-echo "$IPO_COUNT"
